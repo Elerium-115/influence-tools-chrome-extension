b784457f09bab3d2258ac3b5078b30e2f48580d1
Elerium-115 <elerium115+github@gmail.com>
2023-11-25 22:39:09 +0200

    Add various tools

--- globals/tools.js
+++ globals/tools.js
@@ -23,6 +23,8 @@ const tools = [
         items: [
             {title: 'Asteroids Planner', author: 'Elerium115', url: 'https://influence.elerium.dev/asteroids-planner.html'},
             {title: 'Asteroid Management', author: 'Tyrell-Yutani', url: 'https://tyrell-yutani.app/#/asteroid'},
+            {title: 'Asteroid Metadata Lookup by Daharius', author: 'Daharius', url: 'https://influence.daharius.com/AsteroidMetadata/1'},
+            {title: 'Asteroid Metadata Lookup by trevis', author: 'trevis & Dirac', url: 'https://starksight.plus/collection/INFA/1'},
             {title: 'Asteroid Tracker & Visualizer', author: 'Daharius', url: 'https://influence.daharius.com/AsteroidsTracker'},
             {title: 'Adalia Co-orbital Rocks', author: 'RGR', url: 'https://adalia.coorbital.rocks/'},
         ],
@@ -31,9 +33,11 @@ const tools = [
         category_short: 'Crew',
         category: 'Crew Tools',
         items: [
+            {title: 'Crew Member Services', author: 'Tyrell-Yutani', url: 'https://tyrell-yutani.app/#/crew'},
+            {title: 'Crewmate Metadata Lookup by Daharius', author: 'Daharius', url: 'https://influence.daharius.com/CrewmateMetadata/1'},
+            {title: 'Crewmate Metadata Lookup by trevis', author: 'trevis & Dirac', url: 'https://starksight.plus/collection/INFC/1'},
             {title: 'Crew Planner', author: 'Daharius', url: 'https://influence.daharius.com/CrewPlanner'},
             {title: 'Crew Builder', author: 'Grey Area', url: 'https://adalia.academy/POC/index.html'},
-            {title: 'Crew Member Services', author: 'Tyrell-Yutani', url: 'https://tyrell-yutani.app/#/crew'},
             {title: 'Action Planner', author: 'Elerium115', url: 'https://elerium-115.github.io/influence-css/tool-action-planner/'},
         ],
     },
@@ -53,9 +57,9 @@ const tools = [
         category_short: 'Lore',
         category: 'Influence Lore',
         items: [
-            {title: 'The Incident in Sixteen', author: 'Korivak', url: 'https://matthew.debarth.com/influence/the-incident-in-sixteen'}, // individual articles blocked by "medium.com"
             {title: 'Last Night in Space', author: 'Korivak', url: 'https://lastnight.space/'},
-            // {title: 'strwrsfrk Influence Lore', author: 'strwrsfrk', url: 'https://strwrsfrk.medium.com/'}, // blocked by "medium.com"
+            {title: 'Influence Lore by Korivak', author: 'Korivak', url: 'https://matthew.debarth.com/influence/the-incident-in-sixteen'}, // individual articles blocked by "medium.com"
+            {title: 'Influence Lore by strwrsfrk', author: 'strwrsfrk', url: 'https://strwrsfrk.medium.com/'}, // blocked by "medium.com"
         ],
     },
     {
