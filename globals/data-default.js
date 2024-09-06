7de210e81e666e164c85f950c38b978b24f343a1
Elerium-115 <elerium115+github@gmail.com>
2024-09-06 13:25:24 +0300

    Add widget: Process Profits

--- globals/data-default.js
+++ globals/data-default.js
@@ -392,6 +392,7 @@ const toolsDefault = [
 // Widgets snapshot @ 2024-09-06
 const widgetsDefault = [
     {title: 'Shopping List', url: 'https://influence.elerium.dev/widgets/shopping-list.html', default: true},
+    {title: 'Process Profits', url: 'https://influence.elerium.dev/widgets/process-profits.html'},
     {title: 'Marketplace Prices', url: 'https://widgets.starkgames.io/prices/129'},
     {title: 'Private Labels', url: 'https://influence.elerium.dev/widgets/private-labels.html'},
     {title: 'More coming soon...', url: null},
